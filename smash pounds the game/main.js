fde30b217a7cdc39af5157ca2e7d3bfca3aa372d
Solar <94998555+Yandurva08@users.noreply.github.com>
2024-03-14 19:55:03 -0300

    Add files via upload

--- smash pounds the game/main.js
+++ smash pounds the game/main.js
@@ -1,51 +1,75 @@
 alert("Remember! this game is made when i still learing javascript (and i'm still learing), for now i don't know how to save in javascript, so your progress will be gone when you leave this page or refresh this page, now you can go")
 let smashvar = 0
-let lvlsmash = "newbie"
+let smashper = 1
+let smashhs = 0
+var buy = new Audio('buy.ogg');
+var deny = new Audio('deny.ogg');
 function smashfun() {
-	smashvar = 1 + smashvar
-	document.getElementById("smash").innerHTML = "smashes: " + smashvar
-	if (smashvar == 50) {
-	lvlsmash = "smasher"
+var tap = new Audio('tap.ogg');
+  tap.play();
+	smashvar = smashper + smashvar
+	document.getElementById("smash").innerHTML = "Smashes: " + smashvar
+	if (smashvar > smashhs) {
+		smashhs = smashvar
+		document.getElementById("highscore"). innerHTML = "HighScore: " + smashhs
+}
 	}
-	if (smashvar == 100) {
-	lvlsmash = "rizzler"
+let nitrocost = 10
+let nitrocostmult = 1
+let isfive = false
+let spc = 5
+function buynitro() {
+	if (smashvar >= nitrocost) {
+	if (isfive == true) {
+	smashper += spc
+	spc *= 2
+	document.getElementById("npc").innerHTML = "+" + spc
 	}
-	if (smashvar == 200) {
-	lvlsmash = "Zamn"
+	else {
+		smashper += 4
+		isfive = true
+		}
+	console.log("smash per: " + smashper)
+	smashvar -= nitrocost
+	nitrocost *= nitrocostmult *= 2
+	console.log("nitro cost mult: " + nitrocostmult)
+	
+	document.getElementById("costnitro").innerHTML = nitrocost
+	document.getElementById("smash").innerHTML = "Smashes: " + smashvar
+	buy.play()
 	}
-	if (smashvar >= 200) {
-	document.getElementById("titleid").innerHTML = "you smashed him " + smashvar + " times, You should calm down"
+	else {
+		deny.play()
 	}
-	if (smashvar == 269) {
-	lvlsmash = "69, nice"
 	}
-	if (smashvar == 300) {
-	lvlsmash = "maybe you should calm down?"
+let smashpersec = 0
+const spsin = setInterval(function() {
+	smashvar += smashpersec
+	document.getElementById("smash").innerHTML = "Smashes: " + smashvar
+	if (smashvar > smashhs) {
+		smashhs = smashvar
+		document.getElementById("highscore"). innerHTML = "HighScore: " + smashhs
+}
+}, 1000)
+let spscost = 50
+let spscostmult = 1
+let spsamo = 5
+function balling() {
+	if (smashvar >= spscost) {
+		smashpersec += spsamo
+		smashvar -= spscost
+		spscost *= spscostmult *= 2
+		spsamo *= 2
+		document.getElementById("smash").innerHTML = "smashes: " + smashvar
+		document.getElementById("costballing"). innerHTML = spscost
+		document.getElementById("spss").innerHTML = "+" + spsamo
+		buy.play()
+		console.log("sps: " + smashpersec)
 	}
-	if (smashvar == 400) {
-	lvlsmash = "Ok, stop"
+	else {
+		deny.play()
 	}
-	if (smashvar == 500) {
-	lvlsmash = "I'm serious, stop"
 	}
-	if (smashvar == 600) {
-	lvlsmash = "you know he's 14, right?"
-	}
-	if (smashvar == 610) {
-	lvlsmash = "you don't listen to me"
-	}
-	if (smashvar == 700) {
-	lvlsmash = "should i call the police?"
-	}
-	if (smashvar == 900) {
-	lvlsmash = "What is the police number? i forget"
-	}
-	if (smashvar == 911) {
-	lvlsmash = "Thanks for telling me is 911"
-	}
-	if (smashvar == 1000) {
-	lvlsmash = "i have no words"
-	document.getElementById("titleid").innerHTML = "i have no words"
-	}
-	document.getElementById("levelsmash").innerHTML = "level: " + lvlsmash
+function theend() {
+	deny.play()
 	}
